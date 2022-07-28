88ceeab55388afa766ec3cb3cfe459904b249f91
David Serrano <pitgear100@gmail.com>
2022-07-27 20:09:14 -0500

    fix: correccion de variable en el main.contoller

--- src/routes/main.controller.js
+++ src/routes/main.controller.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const dummyRouter  = require('./dummyes/index');
-const pokemonsRouter = require('./pokemonskanto/index');
+const pokemonskantoRouter = require('./pokemonskanto/index');
 const charactersRouter = require('./characters/index')
 // const customersRouter = require('./customers/index');
 
 const routerApi = (app) => {
     // 6.1 Puntos de entrada de la API
     app.use('/dummy', dummyRouter);
-    app.use('/pokemonskanto', pokemonsRouter);
+    app.use('/pokemonskanto', pokemonskantoRouter);
     app.use('/characters', charactersRouter)
     // router.use('/customers', customersRouter);
 };
--- src/routes/pokemonskanto/index.js
+++ src/routes/pokemonskanto/index.js
@@ -3,23 +3,23 @@
  * Capa route, utilizada para el mapeo de los path
  */
  const express = require('express');
- const pokemonsRouter = express.Router();
- const PokemonsServices = require('../../services/pokemons/');
+ const pokemonskantoRouter = express.Router();
+ const PokemonskantoServices = require('../../services/pokemonskanto');
  
- const pokemonService = new PokemonsServices();
+ const pokemonkantoService = new PokemonskantoServices();
  
  // http://localhost:3000/shoes/
  // Query Params: Filtrar información
  // http://localhost:3000/shoes/?page=1&pageSize=10&brand=%22noke%22
  // %20 => espacio en blanco
  // %22 => comillas dobles
- pokemonsRouter.get('/', async (req, res) => {
+ pokemonskantoRouter.get('/', async (req, res) => {
      // 6.1.1: Leer la request
      const { height } = req.query;
      try {
          // 6.1.2: Acceder a la capa service para tener una respuesta
-         const pokemons = await pokemonService.findAll(height);
-         res.status(200).json(pokemons);
+         const pokemonskanto = await pokemonkantoService.findAll(height);
+         res.status(200).json(pokemonskanto);
      } catch(error) {
          // 6.1.3: Si hay un error al acceder al services respondemos un error generico
          res.status(404).json( { message: 'no hay datos'} );
@@ -29,11 +29,11 @@
  
  // Request Param: Son utilizados para ejecutar operaciones sobre un elemento especifico
  // http://localhost:3000/shoes/999
- pokemonsRouter.get('/:id', async (req, res) => {
+ pokemonskantoRouter.get('/:id', async (req, res) => {
      const { id } = req.params;
      try {
-         const foundedPokemon = await pokemonService.findOne(id);
-         res.status(200).send( { message: 'encontrado!', foundedPokemon } );
+         const foundedPokemonkanto = await pokemonkantoService.findOne(id);
+         res.status(200).send( { message: 'encontrado!', foundedPokemonkanto } );
      } catch {
          res.status(404).send({ message: 'ese id no existe' } );
      }
@@ -41,12 +41,12 @@
  
  // http://localhost:3000/shoes/
  // { id: 4, brand: 'pima', type: 600, size: 26 }
- pokemonsRouter.post('/', async (req, res) => {
+ pokemonskantoRouter.post('/', async (req, res) => {
      // 6.1.1: Leer la request
-     const newPokemon = req.body;
+     const newPokemonkanto = req.body;
      try {
          // 6.1.2: Acceder a la capa service para tener una respuesta
-         await pokemonService.create(newPokemon);
+         await pokemonkantoService.create(newPokemonkanto);
          res.status(201).send({message: 'Creacion exitosa'});
      } catch(error) {
          // 6.1.3: Si hay un error al acceder al services respondemos un error generico
@@ -55,11 +55,11 @@
  });
  
  // PARTIAL EDITION: PATCH
- pokemonsRouter.patch('/:id', async (req, res) => {
+ pokemonskantoRouter.patch('/:id', async (req, res) => {
      const body = req.body;
      const { id } = req.params;
      try {
-         await pokemonService.editPartial(id, body);
+         await pokemonkantoService.editPartial(id, body);
          res.status(200).send( { message: 'modificacion patch exitosa!', id } );
      } catch {
          res.status(404).send({ message: 'ese id no existe' } );
@@ -67,11 +67,11 @@
  });
  
  // COMPLETE EDITION: PUT
- pokemonsRouter.put('/:id', async (req, res) => {
+ pokemonskantoRouter.put('/:id', async (req, res) => {
      const body = req.body;
      const { id } = req.params;
      try {
-         await pokemonService.editComplete(id, body);
+         await pokemonkantoService.editComplete(id, body);
          res.status(200).send( { message: 'modificacion put exitosa!', id } );
      } catch {
          res.status(404).send({ message: 'ese id no existe' } );
@@ -79,14 +79,14 @@
  });
  
  // DELETE: DELETE
- pokemonsRouter.delete('/:id', async (req, res) => {
+ pokemonskantoRouter.delete('/:id', async (req, res) => {
      const { id } = req.params;
      try {
-         await pokemonService.delete(id);
+         await pokemonkantoService.delete(id);
          res.status(200).send( { message: 'eliminacion exitosa!' } );
      } catch {
          res.status(404).send({ message: 'ese id no existe' } );
      }
  });
  
- module.exports = pokemonsRouter;
+ module.exports = pokemonskantoRouter;
