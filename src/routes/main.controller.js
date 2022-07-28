6ff94de667fbe002a1641ffa0a3071d8ea2e0778
David Serrano <pitgear100@gmail.com>
2022-07-27 19:59:56 -0500

    feat:modificacion del main controler a la nueva ruta pokemonskanto

--- src/routes/main.controller.js
+++ src/routes/main.controller.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const dummyRouter  = require('./dummyes/index');
-const pokemonsRouter = require('./pokemons/index');
+const pokemonsRouter = require('./pokemonskanto/index');
 const charactersRouter = require('./characters/index')
 // const customersRouter = require('./customers/index');
 
 const routerApi = (app) => {
     // 6.1 Puntos de entrada de la API
     app.use('/dummy', dummyRouter);
-    app.use('/pokemons', pokemonsRouter);
+    app.use('/pokemonskanto', pokemonsRouter);
     app.use('/characters', charactersRouter)
     // router.use('/customers', customersRouter);
 };
