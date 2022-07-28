35642ed0272369708abd62eaf84f406be0c2c66b
David Serrano <pitgear100@gmail.com>
2022-07-27 20:29:25 -0500

    feat: nueva direccion pokemonsjohto en main.controller

--- src/routes/main.controller.js
+++ src/routes/main.controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const dummyRouter  = require('./dummyes/index');
 const pokemonskantoRouter = require('./pokemonskanto/index');
+const pokemonsjohtoRouter = require('./pokemonsjohto/index')
 const charactersRouter = require('./characters/index')
 // const customersRouter = require('./customers/index');
 
@@ -9,6 +10,7 @@ const routerApi = (app) => {
     // 6.1 Puntos de entrada de la API
     app.use('/dummy', dummyRouter);
     app.use('/pokemonskanto', pokemonskantoRouter);
+    app.use('./pokemonsjohto', pokemonsjohtoRouter)
     app.use('/characters', charactersRouter)
     // router.use('/customers', customersRouter);
 };
